e0ad000d6b9f4c37ed717050047aa83f4d61412b
Praveen Jindal <praveenjindal62@gmail.com>
2021-05-14 23:08:20 +0530

    V3

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World - V2!" );
+        System.out.println( "Hello World - V3!" );
     }
 }

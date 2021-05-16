ebe92e52eaeefe417bffc3123a1cd697aa0c32d3
Praveen Jindal <praveenjindal62@gmail.com>
2021-05-16 13:44:16 +0530

    Modified JenkinsfileV2

--- Jenkinsfile-v2.groovy
+++ Jenkinsfile-v2.groovy
@@ -6,62 +6,62 @@ pipeline{
                 echo "Hello Stage 1"
             }
         }
-        stage("Stage1") {
+        stage("Stage2") {
             steps{
                 echo "Hello Stage 1"
             }
         }
-        stage("Stage1") {
+        stage("Stage3") {
             steps{
                 echo "Hello Stage 1"
             }
         }
-        stage("Stage1") {
+        stage("Stage4") {
             steps{
                 echo "Hello Stage 1"
             }
         }
-        stage("Stage1") {
+        stage("Stage5") {
             steps{
                 echo "Hello Stage 1"
             }
         }
-        stage("Stage1") {
+        stage("Stage6") {
             steps{
                 echo "Hello Stage 1"
             }
         }
-        stage("Stage1") {
+        stage("Stage7") {
             steps{
                 echo "Hello Stage 1"
             }
         }
-        stage("Stage1") {
+        stage("Stage8") {
             steps{
                 echo "Hello Stage 1"
             }
         }
-        stage("Stage1") {
+        stage("Stage9") {
             steps{
                 echo "Hello Stage 1"
             }
         }
-        stage("Stage1") {
+        stage("Stage10") {
             steps{
                 echo "Hello Stage 1"
             }
         }
-        stage("Stage1") {
+        stage("Stage11") {
             steps{
                 echo "Hello Stage 1"
             }
         }
-        stage("Stage1") {
+        stage("Stage12") {
             steps{
                 echo "Hello Stage 1"
             }
         }
-        stage("Stage1") {
+        stage("Stage13") {
             steps{
                 echo "Hello Stage 1"
             }
